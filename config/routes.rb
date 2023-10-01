c3b06915454f0a4d2029696b0d656ad4fc519c66
nosukepg1997 <ikotenyoku1030@gmail.com>
2023-10-02 01:43:54 +0900

    デプロイ準備追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'items/index'
+  get 'messages/items'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root to: "items#index"
 end
