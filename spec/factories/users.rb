6e5702b329170ce7baff51bbcd2c0f3e9738f536
nosukepg1997 <ikotenyoku1030@gmail.com>
2023-10-10 14:24:27 +0900

    テストコードの修正
    https://gyazo.com/4344e21ba54f42705c8c25533e18b32b

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname { Faker::Name.nickname }
     email { Faker::Internet.email }
-    password { Faker::Internet.password(min_length: 6, mix_case: true, special_characters: false) }
+    password { '1a' + Faker::Internet.password(min_length: 6, mix_case: true, special_characters: false) }
     password_confirmation { password }
     last_name { "試験のネーム" }
     first_name { "テストの名前" }
